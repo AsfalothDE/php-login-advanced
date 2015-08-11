3aebface1cf24ae9a2717906e173c9d45f7b68e3
Silvia Ulenberg <kontakt@silviaulenberg.de>
2015-08-11 21:24:24 +0200

    Refactoring views from master to bootstrap

--- views/not_logged_in.php
+++ views/not_logged_in.php
@@ -1,13 +1,19 @@
 <h2><?php echo WORDING_LOGIN; ?></h2>
 <form method="post" action="<?php echo $_SERVER['SCRIPT_NAME']; ?>">
-	<label for="user_name"><?php echo WORDING_USERNAME; ?></label>
-	<input id="user_name" type="text" name="user_name" required autofocus/>
-	<label for="user_password"><?php echo WORDING_PASSWORD; ?></label>
-	<input id="user_password" type="password" name="user_password" autocomplete="off" required /><br/>
-	<input type="checkbox" id="user_rememberme" name="user_rememberme" value="1" />
-	<label for="user_rememberme" style=" display:inline;"><?php echo WORDING_REMEMBER_ME; ?></label>
-	<input type="submit" name="login" value="<?php echo WORDING_LOGIN; ?>" />
+    <div class="form-group">
+        <label for="user_name" class="sr-only"><?php echo WORDING_USERNAME; ?></label>
+        <input type="text" name="user_name" id="user_name" class="form-control" placeholder="<?php echo WORDING_USERNAME; ?>" required autofocus>
+    </div>
+    <div class="form-group">
+        <label for="user_password" class="sr-only"><?php echo WORDING_PASSWORD; ?></label>
+        <input type="password" name="user_password" id="user_password" class="form-control" placeholder="<?php echo WORDING_PASSWORD; ?>" required autocomplete="off">
+    </div>
+    <div class="checkbox">
+        <label for="user_rememberme">
+            <input type="checkbox" name="user_rememberme" id="user_rememberme" value="1"> <?php echo WORDING_REMEMBER_ME; ?>
+        </label>
+    </div>
+    <button type="submit" name="login" class="btn btn-lg btn-primary btn-block"><?php echo WORDING_LOGIN; ?></button>
 </form>
-
 <?php echo ($login->config->ALLOW_USER_REGISTRATION ? '<a href="?register">'. WORDING_REGISTER_NEW_ACCOUNT .'</a>&nbsp;&nbsp;|&nbsp;&nbsp;' : ''); ?>
 <a href="?password_reset"><?php echo WORDING_FORGOT_MY_PASSWORD; ?></a>
